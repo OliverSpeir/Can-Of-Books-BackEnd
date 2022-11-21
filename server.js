0c2e9835745a18992735df09763fd62718eb676b
Oliver Speir <oliverspeir9@gmail.com>
2022-11-21 22:24:47 +0000

    added mongoose connect line

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
+mongoose.connect(process.env.DB_URL);
+
+
 app.get('/books', async (request,response) => {
   const filterQuery = {};
   if (request.query.title) {    filterQuery.title = request.query.title;  }
