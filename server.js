c5cd12e74dee1622c3b662f8a8059ca95023dedc
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-11-21 14:17:07 -0800

    finished up to step2 on trello board

--- server.js
+++ server.js
@@ -3,11 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const Book = require('./Models/Book');
 const app = express();
+
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+app.get('/books', async (request,response) => {
+  const filterQuery = {};
+  if (request.query.title) {    filterQuery.title = request.query.title;  }
+  const books = await Book.find(filterQuery);
+  response.send(books);
+})
 
 app.get('/test', (request, response) => {
 
